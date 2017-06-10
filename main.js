56c39bae8485e4152855c9e06fe7c0a907fafe35
Alfredo <gopher.ae@gmail.com>
2017-06-10 13:58:46 -0500

    prevent going backwards

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
 // -------------
-// EVENTS
-// -------------
-document.addEventListener('DOMContentLoaded',function(){
-    game.init();
-    game.start();
-});
-
-document.addEventListener('keydown',function(e){
-    if (e.code == 'ArrowUp') {
-        game.changeSpeed(0, -1);
-    } else if (e.code == 'ArrowDown') {
-        game.changeSpeed(0, 1);
-    } else if (e.code == 'ArrowLeft') {
-        game.changeSpeed(-1, 0);
-    } else if (e.code == 'ArrowRight') {
-        game.changeSpeed(1, 0);
-    }
-});
-
-// -------------
 // MAIN GAME LOGIC
 // -------------
 var game = (function(canvas){
@@ -46,6 +26,9 @@ var game = (function(canvas){
             draw();
         },fps);
     },
+    end = function(){
+
+    },
     draw = function(){
         // clean canvas
         ctx.clearRect(0,0,800,600);
@@ -69,26 +52,76 @@ var game = (function(canvas){
 
         eatFood();
     },
+    updateSpeed = function(newX, newY){
+        speed.x = newX*pixel;
+        speed.y = newY*pixel;
+    },
     createFood = function(){
         food.x = Math.floor((Math.random()*800 + 1)/pixel) * pixel;
         food.y = Math.floor((Math.random()*600 + 1)/pixel) * pixel;
     },
     eatFood = function(){
         distance = {x:headPosition.x-food.x, y:headPosition.y-food.y};
-        console.log('distance from food:', distance);
         if (distance.x == 0 && distance.y == 0) {
             snakeTailSize++;
             createFood();
         }
     },
-    updateSpeed = function(newX, newY){
-        speed.x = newX*pixel;
-        speed.y = newY*pixel;
+    validateDirection = function(keyCode){
+        var currentDirection = '',
+            validCode = true;
+        
+        if (speed.x == -20) {
+            currentDirection = 'ArrowLeft';
+        } else if (speed.x == 20) {
+            currentDirection = 'ArrowRight';
+        } else if (speed.y == -20) {
+            currentDirection = 'ArrowUp';
+        } else if (speed.y == 20) {
+            currentDirection = 'ArrowDown';
+        }
+
+        if (keyCode == 'ArrowLeft' && currentDirection == 'ArrowRight' ||
+            keyCode == 'ArrowRight' && currentDirection == 'ArrowLeft' ||
+            keyCode == 'ArrowUp' && currentDirection == 'ArrowDown' ||
+            keyCode == 'ArrowDown' && currentDirection == 'ArrowUp') {
+            validCode = false;
+        }
+
+        return validCode;
     };
 
     return {
         init:init,
         start:start,
-        changeSpeed:updateSpeed
+        changeSpeed:updateSpeed,
+        validDirection: validateDirection
     };
 })(document.getElementById('game-canvas'));
+
+// -------------
+// EVENTS
+// -------------
+document.addEventListener('DOMContentLoaded',function(){
+    game.init();
+    game.start();
+});
+
+document.addEventListener('keydown',function(e){
+    if (!game.validDirection(e.code)) { return; }
+
+    if (e.code == 'Backspace') { // hold history back
+        e.preventDefault();
+        e.stopPropagation();
+    } else if (e.code == 's') { // game start
+        game.start();
+    } else if (e.code == 'ArrowUp') {
+        game.changeSpeed(0, -1);
+    } else if (e.code == 'ArrowDown') {
+        game.changeSpeed(0, 1);
+    } else if (e.code == 'ArrowLeft') {
+        game.changeSpeed(-1, 0);
+    } else if (e.code == 'ArrowRight') {
+        game.changeSpeed(1, 0);
+    }
+});
